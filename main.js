2a118c4c9cb08768d655d17cac0d3504ff6ee745
simran365 <101072213+simran365@users.noreply.github.com>
2023-03-17 18:49:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+lipstick = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y+10;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
+    }
+}
+
+function draw()
+{
+image(video, 0, 0, 300, 300);
+image(lipstick, noseX, noseY, 40, 40);
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
+
